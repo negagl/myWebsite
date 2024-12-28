61c75c9eacd1f75576fb56d4fc6d678b220a9f41
Nega García <nega_gl@outlook.com>
2024-12-28 16:33:28 -0500

    Created GET Project by ID

--- internal/projects/handlers.go
+++ internal/projects/handlers.go
@@ -3,12 +3,35 @@ package projects
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 func GetProjects(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(projects); err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func GetProjectByID(w http.ResponseWriter, r *http.Request) {
+	idString := r.URL.Path[len("/projects/"):]
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		http.Error(w, "Invalid project ID", http.StatusBadRequest)
 		return
 	}
+
+	for _, project := range projects {
+		if project.ID == id {
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/json")
+			if err := json.NewEncoder(w).Encode(project); err != nil {
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				return
+			}
+			return
+		}
+	}
+
+	http.Error(w, "Project not found", http.StatusNotFound)
 }
--- internal/projects/projects_test.go
+++ internal/projects/projects_test.go
@@ -25,3 +25,52 @@ func TestGetProjects(t *testing.T) {
 		t.Errorf("Unexpected response body: got %q, want %q", rec.Body.String(), expectedResponse)
 	}
 }
+
+func TestGetProjectByID(t *testing.T) {
+	tests := []struct {
+		name             string
+		id               string
+		expectedStatus   int
+		expectedResponse string
+	}{
+		{
+			"Valid ID",
+			"1",
+			http.StatusOK,
+			`{"id":1,"title":"Project 1","description":"This is the first project","url":"https://github.com/project1","status":"started"}`,
+		},
+		{
+			"Invalid ID",
+			"99",
+			http.StatusNotFound,
+			"Project not found",
+		},
+		{
+			"Invalid ID",
+			"wrong",
+			http.StatusBadRequest,
+			"Invalid project ID",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodGet, "/projects/"+test.id, nil)
+			if err != nil {
+				t.Fatalf("Could not create the request: %v", err)
+			}
+
+			rec := httptest.NewRecorder()
+
+			GetProjectByID(rec, req)
+
+			if rec.Code != test.expectedStatus {
+				t.Errorf("Unexpected status code: got %d, want %d", rec.Code, test.expectedStatus)
+			}
+
+			if rec.Body.String() != test.expectedResponse+"\n" {
+				t.Errorf("Unexpected respose body: got %q, want %q", rec.Body.String(), test.expectedResponse)
+			}
+		})
+	}
+}
